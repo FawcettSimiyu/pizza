0901d7b40c1cba5cd55d8b3c9eedbc00d36c5e89
blackbad1 <fawcetteugene@gmail.com>
2019-07-28 10:32:27 +0300

    Add constructor function for the pizza.

--- js/main.js
+++ js/main.js
@@ -3,3 +3,9 @@ function PizzaOrders (){
   this.pizzaTotal = [];
   this.costTotal = 0;
 }
+
+function Piza (toppings,size,chosen) {
+  this.pizzaToppings = toppings;
+  this.pizzaSize = size;
+  this.chosenToppings = chosen;
+}
