77423557bd2bd349be0b6d2f29845edd2368a5ae
blackbad1 <fawcetteugene@gmail.com>
2019-07-28 10:37:11 +0300

    Add cost of toppings prototype.

--- js/main.js
+++ js/main.js
@@ -4,8 +4,16 @@ function PizzaOrders (){
   this.costTotal = 0;
 }
 
-function Piza (toppings,size,chosen) {
+function Pizza (toppings,size,chosen) {
   this.pizzaToppings = toppings;
   this.pizzaSize = size;
   this.chosenToppings = chosen;
+}
+
+Pizza.prototype.costOfToppings = function (chosen, toppings) {
+  for (i=0; i<this.chosenToppings.length;i+=1){
+    if (this.chosenToppings[i].checked){
+      this.pizzaToppings += 1;
+    }
+  }
 }
